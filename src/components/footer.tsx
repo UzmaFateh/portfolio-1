faab71cf800d600ac53c6342ef2f12e05b53247d
UzmaFateh <uzmafateh901@gmail.com>
2024-11-12 23:06:09 +0500

    new addition

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,36 +3,34 @@ export default function Footer() {
   return (
     <div>
       <div>
-       
+
         <div className="footer"  >
-          <div className=" text-left   flex flex-col -ml-6 ">
+          <div className=" text-left">
             <h1 className="font-bold">Contact us:</h1>
             <p>Email:uzmaKhan876@gmail.com</p>
             <p>Phone No:01234567</p>
             <p>United Arab Emarite</p>
           </div><br /><br />
-          <div className="flex flex-col -mt-14 -ml-6">
-              <ul> 
-              <li className="flex"><h1 className="font-bold mt-12">Follow us:</h1></li>
+          <div>
+            <ul className="follow">
+              <li>
+                <h1>Follow us:</h1></li>
               <li>Facebook</li>
               <li>instagram</li>
               <li>linkedInn</li>
-              </ul>
+            </ul>
 
 
 
-           
+
 
           </div>
 
         </div>
 
-        
+
       </div>
-      <h1 className="sm:pl-0 md:pl-40 lg:pl-80
- p-4 pl-8
- bg-black
- text-white">Copyright 2022 Portfolio All Rights Reserved  |  Created By : Uzma Khan</h1>
+      <center><h1 className="copyright">Copyright 2022 Portfolio All Rights Reserved  |  Created By : Uzma Khan</h1></center>
     </div>
   );
 
